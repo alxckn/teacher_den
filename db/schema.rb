9cbcfa3899470a0ca8f79678ca81b8f0bb9f22b0
Alexandre Chakroun <alexandre.chakroun@gmail.com>
2019-03-24 01:02:18 +0100

    Install ActiveStorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,59 +10,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190323232847) do
+ActiveRecord::Schema.define(version: 2019_03_24_000017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "articles", id: :serial, force: :cascade do |t|
     t.integer "parent_id"
-    t.boolean "private",   default: false
-    t.integer "status",    default: 0
-    t.string  "title"
-    t.text    "content"
-    t.index ["parent_id"], name: "index_articles_on_parent_id", using: :btree
+    t.boolean "private", default: false
+    t.integer "status", default: 0
+    t.string "title"
+    t.text "content"
+    t.index ["parent_id"], name: "index_articles_on_parent_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "label"
-    t.string   "displayable_name"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "label"
+    t.string "displayable_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "documents", force: :cascade do |t|
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "file_file_name"
-    t.string   "file_content_type"
-    t.integer  "file_file_size"
+  create_table "documents", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "file_file_name"
+    t.string "file_content_type"
+    t.integer "file_file_size"
     t.datetime "file_updated_at"
-    t.string   "token"
-    t.integer  "category_id"
+    t.string "token"
+    t.integer "category_id"
   end
 
-  create_table "global_vars", force: :cascade do |t|
+  create_table "global_vars", id: :serial, force: :cascade do |t|
     t.string "key"
     t.string "value"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.boolean  "admin",                  default: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
