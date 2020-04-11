3aeb308b04bcad004dea4f01a0db43522b67acae
Alexandre Chakroun <alexandre.chakroun@gmail.com>
2020-04-11 16:20:51 +0200

    Update config to work locally

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 RAILS_IMAGE=docker.pkg.github.com/alxckn/teacher_den/rails:latest
+NGINX_IMAGE=docker.pkg.github.com/alxckn/teacher_den/nginx:latest
 
 docker build -t teacher_den ./rails
 docker tag teacher_den $RAILS_IMAGE
-# docker push $RAILS_IMAGE
+docker push $RAILS_IMAGE
+
+docker build -t teacher_den_nginx ./nginx
+docker tag teacher_den_nginx $NGINX_IMAGE
+docker push $NGINX_IMAGE
