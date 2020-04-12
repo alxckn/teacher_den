c72ce75516225392278459a80c21db5fbb440fdd
Alexandre Chakroun <alexandre.chakroun@gmail.com>
2020-04-13 00:02:34 +0200

    Update build script to use alpine version of rails docker image
    1.17GB => 430MB

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 RAILS_IMAGE=docker.pkg.github.com/alxckn/teacher_den/rails:latest
 NGINX_IMAGE=docker.pkg.github.com/alxckn/teacher_den/nginx:latest
 
-docker build -t teacher_den ./rails
-docker tag teacher_den $RAILS_IMAGE
+docker build -t teacher_den_alpine ./rails -f ./rails/Dockerfile.alpine
+docker tag teacher_den_alpine $RAILS_IMAGE
 docker push $RAILS_IMAGE
 
 docker build -t teacher_den_nginx ./nginx
