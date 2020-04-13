238b89ff91af92decbd125642cd134661f834eec
Alexandre Chakroun <alexandre.chakroun@gmail.com>
2020-04-13 16:06:16 +0200

    Optimize even more alpine docker image
    430MB => 190MB

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 RAILS_IMAGE=docker.pkg.github.com/alxckn/teacher_den/rails:latest
 NGINX_IMAGE=docker.pkg.github.com/alxckn/teacher_den/nginx:latest
 
-docker build -t teacher_den_alpine ./rails -f ./rails/Dockerfile.alpine
-docker tag teacher_den_alpine $RAILS_IMAGE
+docker build -t teacher_den/rails ./rails -f ./rails/Dockerfile.alpine
+docker tag teacher_den/rails $RAILS_IMAGE
 docker push $RAILS_IMAGE
 
-docker build -t teacher_den_nginx ./nginx
-docker tag teacher_den_nginx $NGINX_IMAGE
+# docker build -t teacher_den/upload_doc ./rails -f ./rails/Dockerfile.alpine --target upload_doc
+
+docker build -t teacher_den/nginx ./nginx
+docker tag teacher_den/nginx $NGINX_IMAGE
 docker push $NGINX_IMAGE
